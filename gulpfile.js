cc7fc1fdbc4857cb10585e130a4288bc306156ca
mrmrs <hi@mrmrs.cc>
2014-07-02 19:37:08 -0700

    Updating gulpfile with more better workflow.
    * Update skin classes for grays to be more verbose
    * Update package.json with new devDependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,76 @@
-// Load plugins
+// Gulp tasks for MNML
 
+// Load plugins 
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     watch = require('gulp-watch'),
-    lr    = require('tiny-lr'),
-    server = lr(),
-    livereload = require('gulp-livereload'),
     prefix = require('gulp-autoprefixer'),
     minifyCSS = require('gulp-minify-css'),
     sass = require('gulp-ruby-sass'),
-    imagemin = require('gulp-imagemin'),
-    svgmin = require('gulp-svgmin'),
-    csslint = require('gulp-csslint');
+    csslint = require('gulp-csslint'),
+    browserSync = require('browser-sync'),
+    browserReload = browserSync.reload;
 
 
-// Task to minify all css files in the css directory
-
+// Minify all css files in the css directory
+// Run this in the root directory of the project with `gulp minify-css `
 gulp.task('minify-css', function(){
   gulp.src('./css/*.css')
     .pipe(minifyCSS({keepSpecialComments: 0}))
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('./css/i.min.css'));
 });
-
-
-
-// Task to optmize and minify images
-
-gulp.task('minify-img', function() {
-  return gulp.src('./img/**/*')
-    .pipe((imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('./img'));
-});
-
-
-// Task to optimize and minify svg
-
-gulp.task('minify-svg', function(){
-  gulp.src('./img/svg')
-          .pipe(svgmin())
-          .pipe(gulp.dest('./img/svg'));
-});
-
 
 // Use csslint without box-sizing or compatible vendor prefixes (these
 // don't seem to be kept up to date on what to yell about)
-
 gulp.task('csslint', function(){
   gulp.src('./css/*.css')
     .pipe(csslint({
           'compatible-vendor-prefixes': false,
-          'box-sizing': false
+          'box-sizing': false,
+          'important': false,
+          'known-properties': false
         }))
     .pipe(csslint.reporter());
-
 });
 
-
 // Task that compiles scss files down to good old css
-
 gulp.task('pre-process', function(){
   gulp.src('./sass/i.scss')
       .pipe(watch(function(files) {
         return files.pipe(sass({loadPath: ['./sass/'], style: "compact"}))
           .pipe(prefix())
-          .pipe(gulp.dest('./css/'))
-          .pipe(livereload(server));
+          .pipe(gulp.dest('css'))
+          .pipe(browserSync.reload({stream:true}));
       }));
 });
 
+// Initialize browser-sync which starts a static server also allows for 
+// browsers to reload on filesave
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// Function to call for reloading browsers
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
 
 /*
    DEFAULT TASK
 
- • Process sass and lints outputted css
- • Outputted css is run through autoprefixer
- • Sends updates to any files in directory to browser for
- automatic reloading
+ • Process sass then auto-prefixes and lints outputted css
+ • Starts a server on port 3000
+ • Reloads browsers when you change html or sass files
 
 */
-
-gulp.task('default', function(){
-  gulp.run('pre-process', 'csslint');
-  server.listen(35729, function (err) {
-    gulp.watch(['./sass/*.scss'], function(event) {
-      gulp.run('pre-process', 'csslint');
-    });
-  });
-});
-
-
-gulp.task('production', function(){
-    gulp.run('minify-css', 'minify-img', 'minify-svg');
+gulp.task('default', ['pre-process', 'bs-reload', 'browser-sync'], function(){
+  gulp.start('pre-process', 'csslint');
+  gulp.watch('sass/*.scss', ['pre-process']);
+  gulp.watch('css/i.css', ['bs-reload']);
+  gulp.watch('*.html', ['bs-reload']);
 });
 
