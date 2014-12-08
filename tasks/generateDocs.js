0a41f305393047b6b564dbad6cd92f98df39bdf4
Jeff Kolesky <jeff@kolesky.com>
2014-12-07 18:48:41 -0800

    Adds submodules for generated documentation

--- tasks/generateDocs.js
+++ tasks/generateDocs.js
@@ -14,13 +14,27 @@ var paths = {
 var scssDependencies = [ 'queries', 'colors' ];
 var docModules = [{
   name: 'borders',
-  files: [
-    'borders',
-    'border_colors',
-    'border_radius',
-    'border_style',
-    'border_widths'
-  ],
+  subModules: [{
+    name: 'borders',
+    files: [ 'borders' ],
+    resetSelectors: 'b--black bg-near-white'
+  }, {
+    name: 'border colors',
+    files: [ 'border_colors' ],
+    resetSelectors: 'ba bw1'
+  }, {
+    name: 'border radius',
+    files: [ 'border_radius' ],
+    resetSelectors: 'ba b--black'
+  }, {
+    name: 'border style',
+    files: [ 'border_style' ],
+    resetSelectors: 'ba b--black bg-near-white'
+  }, {
+    name: 'border widths',
+    files: [ 'border_widths' ],
+    resetSelectors: 'ba b--black'
+  }],
   template: 'borders'
 }];
 
@@ -28,56 +42,62 @@ var masterTemplate = fs.readFileSync(paths.masterTemplate).toString();
 
 function generateDocs(done) {
   _.forEach(docModules, function forEachDocModule(docModule) {
-    var files = docModule.files;
-    // Create a temporary scss file to be compiled into the css for this module.
-    var scss = _.map(scssDependencies.concat(files), function createScss(file) {
-      return '@import "' + file + '";';
-    }).join('\n');
-    // TODO: go async
-    var cssText = sass.renderSync({
-      data: scss,
-      includePaths: [ paths.sass ]
-    });
-
-    // Read the css ast, and for each selector in each rule, generate an example.
-    var ast = css.parse(cssText);
-
-    // TODO: go asyn
-    var template =
+    var docModuleTemplate =
       fs.readFileSync(path.join(paths.templates, docModule.template + '.html'))
         .toString();
-    var examples = _.chain(ast.stylesheet.rules)
-      .filter(function isRule(declaration) {
-        return declaration.type === 'rule';
-      })
-      // No need to filter out media queries, because they are one level down in
-      // the syntax tree, and we only filter the top level.
-      .map(function templatizeRule(rule) {
-        var selectors = rule.selectors;
-        var selector = _.chain(selectors)
-          .map(function clearPeriod(selector) {
-            return selector.slice(1);
-          })
-          .values()
-          .join(' ');
-        var selectorForDisplay = selectors.join(', ');
-        var declaration = _.chain(rule.declarations)
-          .map(function declarationToString(declaration) {
-            return declaration.property + ': ' + declaration.value;
-          })
-          .value()
-          .join('; ');
-        var example = _.template(template, {
-          selector: selector,
-          selectorForDisplay: selectorForDisplay,
-          declaration: declaration
-        });
-        return example;
-      })
-      .value();
+    var subModuleData = _.map(docModule.subModules, function toData(subModule) {
+      var files = subModule.files;
+      // Create a temporary scss file to be compiled into the css for this module.
+      var scss = _.map(scssDependencies.concat(files), function createScss(file) {
+        return '@import "' + file + '";';
+      }).join('\n');
+      // TODO: go async
+      var cssText = sass.renderSync({
+        data: scss,
+        includePaths: [ paths.sass ]
+      });
+
+      // Read the css ast, and for each selector in each rule, generate an example.
+      var ast = css.parse(cssText);
+
+      var examples = _.chain(ast.stylesheet.rules)
+        .filter(function isRule(declaration) {
+          return declaration.type === 'rule';
+        })
+        // No need to filter out media queries, because they are one level down
+        // in the syntax tree, and we only filter the top level.
+        .map(function templatizeRule(rule) {
+          var selectors = rule.selectors;
+          var selector = _.chain(selectors)
+            .map(function clearPeriod(selector) {
+              return selector.slice(1);
+            })
+            .values()
+            .join(' ');
+          var selectorForDisplay = selectors.join(', ');
+          var declaration = _.chain(rule.declarations)
+            .map(function declarationToString(declaration) {
+              return declaration.property + ': ' + declaration.value;
+            })
+            .value()
+            .join('; ');
+          var example = _.template(docModuleTemplate, {
+            resetSelector: subModule.resetSelectors,
+            selector: selector,
+            selectorForDisplay: selectorForDisplay,
+            declaration: declaration
+          });
+          return example;
+        })
+        .value();
+      return {
+        name: subModule.name,
+        examples: examples
+      };
+    });
     var moduleTemplate = _.template(masterTemplate, {
-      module_name: docModule.name,
-      examples: examples
+      moduleName: docModule.name,
+      subModules: subModuleData
     });
     // TODO: go async
     fs.writeFileSync(path.join(paths.docs, docModule.name, 'index.html'),
@@ -86,7 +106,5 @@ function generateDocs(done) {
 
   return done();
 }
-
-generateDocs(function() {});
 
 module.exports = generateDocs;
