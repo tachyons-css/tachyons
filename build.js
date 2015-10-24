ab11ae313a49fdaf7ef4639b182c32509502abd4
John Otander <johnotander@gmail.com>
2015-10-24 12:28:23 -0600

    Finish basic build script to copy over modules from their sources

--- build.js
+++ build.js
@@ -1,7 +1,40 @@
 var pkg = require('./package.json')
+var copy = require('copy-files')
+
+var ignoredModules = [
+  'tachyons-cli',
+  'tachyons-queries'  // For now, need to update to play nice
+]
 
 var modules = Object.keys(pkg.devDependencies).filter(function (module) {
-  return module !== 'tachyons-cli'
+  return (
+    isTachyonsModule(module) ||
+    module === 'normalize.css'
+  ) && ignoredModules.indexOf(module) == -1
 })
 
-console.log(modules)
+var files = {}
+modules.forEach(function (module) {
+  var moduleLocation = isTachyonsModule(module) ?
+    __dirname + '/node_modules/' + module + '/src/' + module + '.css' :
+    __dirname + '/node_modules/' + module + '/' + module
+
+  var moduleName = module.replace(/(tachyons-|\.css)/, '')
+  files['_' + moduleName + '.css'] = moduleLocation
+})
+
+copy({
+  files: files,
+  dest: __dirname + '/src',
+  overwrite: true
+}, function (err) {
+  if (err) {
+    console.error('Error occurred copying files')
+    console.error(err)
+    process.exit(1)
+  }
+})
+
+function isTachyonsModule (module) {
+  return module.indexOf('tachyons') !== -1
+}
