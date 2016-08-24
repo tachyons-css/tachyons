27ca206faa9e278b0f5eb22ff465e8c33c348daf
John Otander <johnotander@gmail.com>
2016-08-24 15:25:07 -0600

    Add update script

--- build/index.js
+++ build/index.js
@@ -8,10 +8,14 @@ const pattern = [
   '!src/_normalize.css'
 ]
 
-globby(pattern).then(paths => {
-  paths.forEach(path => {
-    const name = path.replace('src/_', '').replace('.css', '')
-    const css = fs.readFileSync(`../${path}`, 'utf8')
-    writeFileSync(`../packages/${name}/src/tachyons-${name}.css`, css)
-  })
-})
+globby(pattern)
+  .then(paths => paths.forEach(processPackage))
+  .catch(console.error)
+
+const processPackage = path => {
+  const name = path.replace('src/_', '').replace('.css', '')
+  const css = fs.readFileSync(path, 'utf8')
+
+  console.log(`building ${path}`)
+  fs.writeFileSync(`packages/tachyons-${name}/src/tachyons-${name}.css`, css)
+}
