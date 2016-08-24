afe34e59ec6ef7f4516730a134a54810797f2916
John Otander <johnotander@gmail.com>
2016-08-24 15:40:07 -0600

    Adjust build to add media queries

--- build/index.js
+++ build/index.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const globby = require('globby')
+const stripComments = require('strip-css-comments')
 
 const pattern = [
   'src/**/*.css',
   '!src/tachyons.css',
   '!src/_debug_children.css',
-  '!src/_normalize.css'
+  '!src/_normalize.css',
+  '!src/_media-queries.css'
 ]
+
+const mq = stripComments(fs.readFileSync('src/_media-queries.css', 'utf8'))
 
 globby(pattern)
   .then(paths => paths.forEach(processPackage))
@@ -14,7 +18,11 @@ globby(pattern)
 
 const processPackage = path => {
   const name = path.replace('src/_', '').replace('.css', '')
-  const css = fs.readFileSync(path, 'utf8')
+  let css = fs.readFileSync(path, 'utf8')
+
+  if (/@media/.test(css)) {
+    css = mq + '\n' + css
+  }
 
   console.log(`building ${path}`)
   fs.writeFileSync(`packages/tachyons-${name}/src/tachyons-${name}.css`, css)
