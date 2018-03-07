6009847ee44ff1596e66b54e8179799401ab3204
John Otander <johnotander@gmail.com>
2018-03-07 15:29:32 -0700

    Tweak formatting

---
+++ docs/ui/FontSizes.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default () => (
+  <div className="w-100 overflow-x-auto pb4">
+    <table>
+      <tbody>
+        <tr>
+          <td className="tc f-headline b br b--black-10 v-base">A</td>
+          <td className="tc ph2 br f-subheadline b br b--black-10 v-base">A</td>
+          <td className="tc f1 b br b--black-10 v-base">A</td>
+          <td className="tc f2 b br b--black-10 v-base">A</td>
+          <td className="tc f3 b br b--black-10 v-base">A</td>
+          <td className="tc f4 b br b--black-10 v-base">A</td>
+          <td className="tc f5 b br b--black-10 v-base">A</td>
+          <td className="tc f6 b b--black-10 v-base">A</td>
+        </tr>
+        <tr>
+          <td className="tc gray f6 fw2 pr2 bt br b--black-10 pt1">
+            6rem (96px)
+          </td>
+          <td className="tc gray f6 fw2 ph2 bt br b--black-10 pt1">
+            5rem (80px)
+          </td>
+          <td className="tc gray f6 fw2 ph2 bt br b--black-10 pt1">
+            3rem (48px)
+          </td>
+          <td className="tc gray f6 fw2 ph2 bt br b--black-10 pt1">
+            2.25rem (36px)
+          </td>
+          <td className="tc gray f6 fw2 ph2 bt br b--black-10 pt1">
+            1.5rem (24px)
+          </td>
+          <td className="tc gray f6 fw2 ph2 bt br b--black-10 pt1">
+            1.25rem (20px)
+          </td>
+          <td className="tc gray f6 fw2 ph2 bt br b--black-10 pt1">
+            1rem (16px)
+          </td>
+          <td className="tc gray f6 fw2 pl2 bt b--black-10 pt1">
+            .875rem (14px)
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+)
--- docs/ui/withDocLayout.js
+++ docs/ui/withDocLayout.js
@@ -4,14 +4,15 @@ import { ComponentsProvider } from '@compositor/markdown'
 import Layout from './Layout'
 import Container from './Container'
 import NavElements from './NavElements'
+import data from '../data.json'
 import { md } from './'
 
 export default Component => ({ title, ...props }) => (
   <ComponentsProvider components={md}>
     <Layout title={title}>
       <NavElements />
-      <Container padding={true} className='black-70'>
-        <Component {...props} />
+      <Container padding={true} className="black-70">
+        <Component data={data} {...props} />
       </Container>
     </Layout>
   </ComponentsProvider>
