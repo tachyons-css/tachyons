a8d655d915ca0f6bfb37b75ec9c1ef8ece5c80eb
John Otander <johnotander@gmail.com>
2018-03-07 14:21:43 -0700

    Add live editor support

---
+++ docs/ui/Editor.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import ContentEditable from 'react-contenteditable'
+import he from 'he'
+
+const isDotHTML = (cx = '') => /language-\.html/.test(cx)
+
+class Editor extends Component {
+	constructor (props) {
+		super()
+
+		this.state = {
+      shouldLiveEdit: isDotHTML(props.className),
+			html: props.children[0] || ''
+		}
+	}
+
+  handleChange = e => {
+    this.setState({
+      html: e.target.value
+    })
+  }
+
+	render () {
+		if (!this.state.shouldLiveEdit) {
+			return <code {...this.props} />
+		}
+
+		return (
+			<div>
+        <textarea
+          rows={1}
+          className='w-100 bn code black-70 ph0 mb2 input-reset'
+          value={this.state.html}
+          onChange={this.handleChange}
+        />
+				<div
+          dangerouslySetInnerHTML={{
+            __html: he.decode(this.state.html)
+          }}
+        />
+			</div>
+		)
+	}
+}
+
+export default Editor
