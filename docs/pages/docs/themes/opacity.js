2bcb1786b90bc4109dae37d3fecc0777a6084a4b
John Otander <johnotander@gmail.com>
2018-03-09 13:02:47 -0700

    Add opacity docs

---
+++ docs/pages/docs/themes/opacity.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import withDocLayout from '../../../ui/withDocLayout'
+import Opacity from '../../../themes/opacity.md'
+
+export default withDocLayout(Opacity, {
+  title: 'Opacity / Themes / Docs / TACHYONS',
+  subNav: 'themes',
+  modules: ['tachyons-opacity'],
+  editUrl: 'https://github.com/tachyons-css/tachyons/edit/master/docs/themes/opacity.md'
+})
