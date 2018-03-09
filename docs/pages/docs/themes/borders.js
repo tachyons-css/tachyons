b0e9accc3fd0ba9ff192585d0700d381daa611ca
John Otander <johnotander@gmail.com>
2018-03-09 13:28:26 -0700

    Add border docs

---
+++ docs/pages/docs/themes/borders.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import withDocLayout from '../../../ui/withDocLayout'
+import Borders from '../../../themes/borders.md'
+
+export default withDocLayout(Borders, {
+  title: 'Borders / Themes / Docs / TACHYONS',
+  subNav: 'themes',
+  modules: ['tachyons-borders', 'tachyons-border-widths', 'tachyons-border-style', 'tachyons-border-radius', 'tachyons-border-colors'],
+  editUrl: 'https://github.com/tachyons-css/tachyons/edit/master/docs/themes/borders.md'
+})
