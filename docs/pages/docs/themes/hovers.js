8c84f1afd5c5fe3eb78d0be69084feb53c3fc825
rosew <rose@parksidesoftware.com>
2018-03-09 23:48:49 -0700

    Port hover documentation

---
+++ docs/pages/docs/themes/hovers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import withDocLayout from '../../../ui/withDocLayout'
+import Hovers from '../../../themes/hovers.md'
+
+export default withDocLayout(Hovers, {
+  title: 'Hovers / Themes / Docs / TACHYONS',
+  subNav: 'themes',
+  modules: ['tachyons-hovers'],
+  editUrl: 'https://github.com/tachyons-css/tachyons/edit/master/docs/themes/hovers.md'
+})
