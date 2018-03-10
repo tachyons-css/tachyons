ee6c586c84a04c363c911d5fb0568a33c90cdcbe
John Otander <johnotander@gmail.com>
2018-03-09 18:19:30 -0700

    Add height docs

---
+++ docs/pages/docs/layout/heights.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import withDocLayout from '../../../ui/withDocLayout'
+import Heights from '../../../layout/heights.md'
+
+export default withDocLayout(Heights, {
+  title: 'Heights / Layout / Docs / TACHYONS',
+  modules: ['tachyons-heights'],
+  subNav: 'layout',
+  editUrl: 'https://github.com/tachyons-css/tachyons/edit/master/docs/layout/heights.md'
+})
