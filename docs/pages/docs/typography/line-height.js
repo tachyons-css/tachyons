63c2be529e83db00722bdd11730dec8cd3fb90ca
rosew <rose@parksidesoftware.com>
2018-03-14 16:33:37 -0600

    Setup the structure for the typography pages, but the md files still need content. Add the content for measure.md

---
+++ docs/pages/docs/typography/line-height.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import withDocLayout from '../../../ui/withDocLayout'
+import LineHeight, { meta } from '../../../typography/line-height.md'
+
+export default withDocLayout(LineHeight, meta)
