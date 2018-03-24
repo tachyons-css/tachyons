bd205b92642a9dbe8f0cdf912cb56e5d41d8623d
John Otander <johnotander@gmail.com>
2018-03-24 10:46:38 -0600

    Nuke cruft

--- docs/pages/blog/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-import withBlogLayout from '../../ui/withBlogLayout'
-import BlogIndex from '../../blog/index.md'
-
-export default withBlogLayout(BlogIndex)
--- docs/pages/blog/v5.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-import withBlogLayout from '../../ui/withBlogLayout'
-import Blog from '../../blog/v5.md'
-
-export default withBlogLayout(Blog)
