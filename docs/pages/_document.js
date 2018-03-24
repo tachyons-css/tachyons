0a558e32f200dce93594697d523cbcdb21102ff4
John Otander <johnotander@gmail.com>
2018-03-24 10:41:03 -0600

    Use next document component to remedy FOUC

---
+++ docs/pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class _Document extends Document {
+  render() {
+    return (
+      <html className='min-h-100'>
+        <Head>
+          <link rel="stylesheet" href="https://file-jyzsebnxow.now.sh" />
+          <link rel="stylesheet" href="https://file-zlngimivyb.now.sh" />
+        </Head>
+        <body className='sans-serif min-h-100'>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
